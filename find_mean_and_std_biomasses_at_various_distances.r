98053488a5ab5aba1cd8eb11f3cecde0c45906bd
Jason Steelman <jasonsteelmancoder@gmail.com>
2024-11-07 10:43:22 -0500

    find the means and standard deviations

--- find_mean_and_std_biomasses_at_various_distances.r
+++ find_mean_and_std_biomasses_at_various_distances.r
@@ -7,9 +7,16 @@
 # read the csv
 input_data <- read.csv('C:/Users/js81535/Desktop/lidar_scan_calibration/HEF Biomass 2024.csv') 
 
+# add a column with the distance from the origin
+distances_list <- c()
+for (i in 1:nrow(input_data)) {
+  distances_list <- c(distances_list, as.double(gsub('[NESW]', '', input_data$Clip.Plot[[i]])))
+}
+input_data$distance_from_origin <- distances_list 
+
 # make lists to populate the indexing columns
 stratum_col <- c(rep('0-30', 99), rep('30-100', 99))
-biomass_type_col <- c(rep('1000hr', 9), rep('100hr', 9), rep('10hr', 9), rep('1hr', 9), rep('CL', 9), rep('ETE', 9), rep('FL', 9), rep('PC', 9), rep('PN', 9), rep('Wlit-BL', 9), rep('Wlive-BL', 9), rep('1000hr', 9), rep('100hr', 9), rep('10hr', 9), rep('1hr', 9), rep('CL', 9), rep('ETE', 9), rep('FL', 9), rep('PC', 9), rep('PN', 9), rep('Wlit-BL', 9), rep('Wlive-BL', 9))
+biomass_type_col <- c(rep('X1000hr', 9), rep('X100hr', 9), rep('X10hr', 9), rep('X1hr', 9), rep('CL', 9), rep('ETE', 9), rep('FL', 9), rep('PC', 9), rep('PN', 9), rep('Wlit.BL', 9), rep('Wlive.BL', 9), rep('X1000hr', 9), rep('X100hr', 9), rep('X10hr', 9), rep('X1hr', 9), rep('CL', 9), rep('ETE', 9), rep('FL', 9), rep('PC', 9), rep('PN', 9), rep('Wlit.BL', 9), rep('Wlive.BL', 9))
 plt_col <- c()
 # one set of plots for every combination of stratum and biomass
 for (i in 1:22) {
@@ -25,12 +32,24 @@ for (i in 1:66) {
 
 # build data frame with one row for each unique combination of stratum, biomass, plot, and distance
 output <- data.frame(stratum_col, biomass_type_col, plt_col, distance_col)
-# name the columns for readability
-stratum_id <- 1
-biomass_id <- 2
-plot_id <- 3
-distance_id <- 4
 
+means <- c()
+stds <- c()
 for (i in 1:198) {
-  print(output[i, stratum_id])
+  stratum <- output[i, 1]
+  biomass <- output[i, 2]
+  plot <- output[i, 3]
+  distance <- output[i, 4]
+  
+  relevant_data <- subset(input_data, Stratum == stratum & Macroplot == plot & distance_from_origin == distance, select = biomass)
+  mean_biomass <- mean(relevant_data[[1]])
+  std_of_biomass <- sd(relevant_data[[1]])
+  means <- c(means, mean_biomass)  
+  stds <- c(stds, std_of_biomass)
 }
+
+#print(means)
+print(stds)
+
+
+
