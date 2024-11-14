2c72e4aa34070ddfb20bbc72fb1f2a6ecaa4c825
Jason Steelman <jasonsteelmancoder@gmail.com>
2024-11-14 14:53:49 -0500

    plot the width of the confidence interval by the number of clip plots

---
+++ plot_width_of_confidence_interval_by_num_samples.py
@@ -0,0 +1,22 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
+Z = 1.96            # 95% confidence interval
+sigma = 13          # the standard deviation of the biomass on observed clip plots
+
+n = np.linspace(1, 24, 24)
+W = np.sqrt((4 * (Z**2) * (sigma**2)) / (n))
+print(W)
+
+plt.plot(n, W)
+plt.title("Width of the Confidence Interval \nfor the estimated mean biomass per quarter meter squared".title())
+plt.ylim(bottom=0)
+plt.ylabel("Width of Confidence Interval")
+plt.xlabel("Number of Clip Plots")
+plt.show()
+
+plt.clf()
+distr = np.random.normal(61, 3.5, 5000)
+plt.hist(distr, bins=100)
+plt.xlim(left=0)
+plt.show()
