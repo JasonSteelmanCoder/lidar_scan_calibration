a0ca9daa1c4816105b69a1f651df19d91495bb16
Jason Steelman <jasonsteelmancoder@gmail.com>
2024-11-19 16:26:08 -0500

    change formula for calculating weights

--- find_wieghts_matrix_for_weighted_mean_biomass.py
+++ find_wieghts_matrix_for_weighted_mean_biomass.py
@@ -11,14 +11,26 @@ import matplotlib.pyplot as plt
 
 load_dotenv()
 
-# USER: type the autocorrelation range of the biomass type here
-autocorrelation_range = 2.8
-# USER: put the strength of the weighting here
-weighting_strength = 0.5
 # USER: input the path to your data here
 input_data_path = f"C:/Users/{os.getenv("MS_USER_NAME")}/Desktop/lidar_scan_calibration/HEF Biomass 2024.csv"
 # USER: input your output location here
 output_data_path = f"C:/Users/{os.getenv("MS_USER_NAME")}/Desktop/lidar_scan_calibration/clip_plot_weights_for_10h.csv"
+
+# these autocorreletion ranges are based on the variogram models
+autocorrelation_ranges = {
+    "X1000hr": 100,
+    "X100hr": 2.1,
+    "X10hr": 2.8,
+    "X1hr": 5.1,
+    "CL": 0.92,
+    "ETE": 3.2,
+    "FL": 2.2,
+    "PC": 7.1,
+    "PN": 4.8,
+    "Wlit.BL": 0.64,
+    "Wlive.BL": 68
+}
+
 
 # transform input_data into by-macroplot, with high and low strata summed into one biomass value per clip plot
 input_data = pd.read_csv(input_data_path)
@@ -29,50 +41,38 @@ macroplot3 = grouped_data[grouped_data["Macroplot"] == 3]
 macroplot2.reset_index(inplace=True)
 macroplot3.reset_index(inplace=True)
 
-# initialize dictionaries
-inverse_distances = {}
-inverse_distance_sums = {}
+
+autocorrelation_range = autocorrelation_ranges["X10hr"]
+
+plot = macroplot1
+
+distances_by_clip_plot = {}
+curved_distances_by_clip_plot = {}
 weights = {}
 
-output = pd.DataFrame(columns=["macroplot", "clip_plot", "weight"])
+for k in range(24):
+    clip_plot = plot["Clip Plot"][k]
+    list_coords = ast.literal_eval(plot["Coordinates"][k])
+    x = list_coords[0]
+    y = list_coords[1]
+    
+    distances = []
+    curved_distances = []
 
-plots = [macroplot1, macroplot2, macroplot3]
+    for coord_pair in plot["Coordinates"]:
+        neighbor_coords = ast.literal_eval(coord_pair)
+        neighbor_x = neighbor_coords[0]
+        neighbor_y = neighbor_coords[1]
 
-for k in range(len(plots)):
-    # for each clip plot in the macroplot, find the sum of the inverses of each distance to another clip plot within the autocorrelation range
-    for i in range(24):
-        clip_plot = plots[k]["Clip Plot"][i]                        # the name of the clip plot
-        list_coords = ast.literal_eval(plots[k]["Coordinates"][i])      
-        x = list_coords[0]
-        y = list_coords[1]
-        this_inverse_distances = []         # a list to store all of the inverse distances for this clip plot
-        
-        for inner_pair in plots[k]["Coordinates"]:                  # go through all of the surrounding clip plots
-            inner_list_coords = ast.literal_eval(inner_pair)
-            inner_x = inner_list_coords[0]
-            inner_y = inner_list_coords[1]
+        distance = np.sqrt((x - neighbor_x)**2 + (y - neighbor_y)**2)
+        if distance < autocorrelation_range and distance > 0:
+            distances.append(distance)
+            curved_distances.append(np.exp((-4 * distance**2) / autocorrelation_range**2))
 
-            distance = np.sqrt((x - inner_x)**2 + (y - inner_y)**2)
-            if distance < autocorrelation_range and distance != 0:          # distances beyond the autocorrelation range don't have an effect
-                this_inverse_distances.append(1 / distance)
+    distances_by_clip_plot[clip_plot] = distances
+    curved_distances_by_clip_plot[clip_plot] = curved_distances
+    weights[clip_plot] = 1 / (1 + sum(curved_distances))
 
-        inverse_distances[clip_plot] = sorted(this_inverse_distances, reverse=True)        
-        inverse_distance_sums[clip_plot] = sum(this_inverse_distances)
-        weights[clip_plot] = 1 / sum(this_inverse_distances)**weighting_strength               # change this exponent to adjust the strength of the weights
-
-    # print(inverse_distances)
-
-    # sorted_inverse_distance_sums = sorted(inverse_distance_sums.items(), key= lambda item: item[1])
-    # for item in sorted_inverse_distance_sums:
-    #     print(item)
-
-    # sorted_weights = sorted(weights.items(), key= lambda item: item[1])
-    # for item in sorted_weights:
-    #     print(item)
-
-    for item in weights.items():
-        output.loc[len(output)] = k + 1, item[0], item[1]
-
-print(output)
-
-output.to_csv(output_data_path, index=False)
+# print(distances_by_clip_plot)
+# print(curved_distances_by_clip_plot)
+print(weights)
