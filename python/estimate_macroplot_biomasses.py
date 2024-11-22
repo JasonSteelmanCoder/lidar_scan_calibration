f3186d886104bd357859b688202efd6ed1b50c0f
Jason Steelman <jasonsteelmancoder@gmail.com>
2024-11-22 13:48:58 -0500

    find weighted total estimates with new weights

--- python/estimate_macroplot_biomasses.py
+++ python/estimate_macroplot_biomasses.py
@@ -6,10 +6,10 @@ import pandas as pd
 load_dotenv()
 
 # USER: put the locations of your input here. You need to input weighted and unweighted means
-unweighted_input = f"C:/Users/{os.getenv("MS_USER_NAME")}/Desktop/lidar_scan_calibration/mean_and_std_biomasses_by_macroplot_and_type.csv"
-weighted_input = f"C:/Users/{os.getenv("MS_USER_NAME")}/Desktop/lidar_scan_calibration/weighted_mean_and_std_biomasses_by_macroplot_and_type.csv"
+unweighted_input = f"C:/Users/{os.getenv("MS_USER_NAME")}/Desktop/lidar_scan_calibration/csv_data/mean_and_std_biomasses_by_macroplot_and_type.csv"
+weighted_input = f"C:/Users/{os.getenv("MS_USER_NAME")}/Desktop/lidar_scan_calibration/csv_data/weighted_mean_and_std_biomasses_by_macroplot_and_type.csv"
 # USER: enter your output location here.
-output_path = f"C:/Users/{os.getenv("MS_USER_NAME")}/Desktop/lidar_scan_calibration/macroplot_biomass_estimations.csv"
+output_path = f"C:/Users/{os.getenv("MS_USER_NAME")}/Desktop/lidar_scan_calibration/csv_data/macroplot_biomass_estimations.csv"
 
 # make data frames and add columns with estimations
 unweighted_df = pd.read_csv(unweighted_input)
@@ -20,8 +20,8 @@ weighted_df["weighted_total_biomass"] = weighted_df["weighted_mean_biomass"] * 1
 
 # make an output data frame to store the estimated biomasses
 estimated_total_biomasses = pd.DataFrame(data={
-    "macroplot": unweighted_df["macroplot"],
-    "biomass_type": unweighted_df["biomass_type"],
+    "macroplot": weighted_df["macroplot"],
+    "biomass_type": weighted_df["biomass_type"],
     "total_biomass": unweighted_df["total_biomass"], 
     "weighted_total_biomass": weighted_df["weighted_total_biomass"]
 })
@@ -29,4 +29,4 @@ estimated_total_biomasses = pd.DataFrame(data={
 print(estimated_total_biomasses)
 
 # save to csv
-estimated_total_biomasses.to_csv(output_path, index=False)
+# estimated_total_biomasses.to_csv(output_path, index=False)
