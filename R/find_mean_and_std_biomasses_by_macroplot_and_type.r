c20690e0b178abfba0738a58fc4f75573f35c785
Jason Steelman <jasonsteelmancoder@gmail.com>
2024-11-22 13:45:57 -0500

    find unweighted totals for total biomass

--- R/find_mean_and_std_biomasses_by_macroplot_and_type.r
+++ R/find_mean_and_std_biomasses_by_macroplot_and_type.r
@@ -6,9 +6,9 @@
 library(dplyr)
 
 # USER: write your input csv location here
-input_data <- read.csv('C:/Users/js81535/Desktop/lidar_scan_calibration/HEF Biomass 2024.csv') 
+input_data <- read.csv('C:/Users/js81535/Desktop/lidar_scan_calibration/csv_data/HEF Biomass 2024.csv') 
 # USER: write you output location here
-output_location <- 'C:/Users/js81535/Desktop/lidar_scan_calibration/mean_and_std_biomasses_by_macroplot_and_type.csv'
+output_location <- 'C:/Users/js81535/Desktop/lidar_scan_calibration/csv_data/mean_and_std_biomasses_by_macroplot_and_type.csv'
 
 # combine the strata into one biomass per unique combination of clip plot and biomass type
 low_df <- subset(input_data, Stratum == '0-30', select = c("Macroplot", "Clip.Plot", "X1000hr", "X100hr", "X10hr", "X1hr", "CL", "ETE", "FL", "PC", "PN", "Wlit.BL", "Wlive.BL"))
@@ -26,27 +26,20 @@ by_clip_plot_df$PC <- by_clip_plot_df$PC.x + by_clip_plot_df$PC.y
 by_clip_plot_df$PN <- by_clip_plot_df$PN.x + by_clip_plot_df$PN.y
 by_clip_plot_df$Wlit.BL <- by_clip_plot_df$Wlit.BL.x + by_clip_plot_df$Wlit.BL.y
 by_clip_plot_df$Wlive.BL <- by_clip_plot_df$Wlive.BL.x + by_clip_plot_df$Wlive.BL.y
+by_clip_plot_df$total_biomass <- by_clip_plot_df$X1000hr.x + by_clip_plot_df$X1000hr.y + by_clip_plot_df$X100hr.x + by_clip_plot_df$X100hr.y + by_clip_plot_df$X10hr.x + by_clip_plot_df$X10hr.y + by_clip_plot_df$X1hr.x + by_clip_plot_df$X1hr.y + by_clip_plot_df$CL.x + by_clip_plot_df$CL.y + by_clip_plot_df$ETE.x + by_clip_plot_df$ETE.y + by_clip_plot_df$FL.x + by_clip_plot_df$FL.y + by_clip_plot_df$PC.x + by_clip_plot_df$PC.y + by_clip_plot_df$PN.x + by_clip_plot_df$PN.y + by_clip_plot_df$Wlit.BL.x + by_clip_plot_df$Wlit.BL.y + by_clip_plot_df$Wlive.BL.x + by_clip_plot_df$Wlive.BL.y
 
 
 # purge redundant columns
-by_clip_plot_df <- by_clip_plot_df[, c("Macroplot", "Clip.Plot", "X1000hr", "X100hr", "X10hr", "X1hr", "CL", "ETE", "FL", "PC", "PN", "Wlit.BL", "Wlive.BL")]
-
-# drop 2.5m clip plots (they are spatially autocorrelated for all biomass types)
-by_clip_plot_df <- by_clip_plot_df %>%
-  filter(grepl("\\.", Clip.Plot) == FALSE)
-#print(by_clip_plot_df)
-
+by_clip_plot_df <- by_clip_plot_df[, c("Macroplot", "Clip.Plot", "X1000hr", "X100hr", "X10hr", "X1hr", "CL", "ETE", "FL", "PC", "PN", "Wlit.BL", "Wlive.BL", "total_biomass")]
 
 # initialize output columns
-macroplot <- c(rep(1, 11), rep(2, 11), rep(3, 11))
-biomass_type <- c(rep(c("X1000hr", "X100hr", "X10hr", "X1hr", "CL", "ETE", "FL", "PC", "PN", "Wlit.BL", "Wlive.BL"), 3))
+macroplot <- c(rep(1, 12), rep(2, 12), rep(3, 12))
+biomass_type <- c(rep(c("X1000hr", "X100hr", "X10hr", "X1hr", "CL", "ETE", "FL", "PC", "PN", "Wlit.BL", "Wlive.BL", "total_biomass"), 3))
 mean_biomass <- c()
 standard_deviation <- c()
-# autocorrelation ranges are hard coded.
-autocorrelation_range <- c(rep(c(100, 2.1, 2.8, 5.1, 0.92, 3.2, 2.2, 7.1, 4.8, 0.64, 68), 3))
 
 # populate output columns
-for (i in 1:33) {              # there are 33 unique combinations of macroplot and biomass type
+for (i in 1:36) {              # there are 33 unique combinations of macroplot and biomass type
   biomass_values <- subset(by_clip_plot_df, Macroplot == macroplot[[i]], select = biomass_type[[i]])
   this_mean <- sum(biomass_values) / 24         # there are 24 values per plot for each biomass type
   mean_biomass <- c(mean_biomass, this_mean)
@@ -57,10 +50,10 @@ for (i in 1:33) {              # there are 33 unique combinations of macroplot a
 }
 
 # build output data frame
-output <- data.frame(macroplot, biomass_type, mean_biomass, standard_deviation, autocorrelation_range)
+output <- data.frame(macroplot, biomass_type, mean_biomass, standard_deviation)
 
 print(output)
 
 # save the output to csv
-write.csv(output, output_location, row.names = FALSE)
+#write.csv(output, output_location, row.names = FALSE)
 
