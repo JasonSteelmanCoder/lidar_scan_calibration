1bf02714c1c3ebf897fae2b2a157cd1e9989b80b
Jason Steelman <jasonsteelmancoder@gmail.com>
2024-11-22 11:07:26 -0500

    find weighted mean biomass and standard dev w new weights

--- R/find_weighted_mean_and_std_biomasses_by_macroplot_and_type.r
+++ R/find_weighted_mean_and_std_biomasses_by_macroplot_and_type.r
@@ -5,12 +5,12 @@
 library(dplyr)
 
 # USER: write your input csv location here
-input_data <- read.csv('C:/Users/js81535/Desktop/lidar_scan_calibration/HEF Biomass 2024.csv')
-input_weights <- read.csv('C:/Users/js81535/Desktop/lidar_scan_calibration/clip_plot_weights.csv')
+input_data <- read.csv('C:/Users/js81535/Desktop/lidar_scan_calibration/csv_data/HEF Biomass 2024.csv')
+input_weights <- read.csv('C:/Users/js81535/Desktop/lidar_scan_calibration/csv_data/clip_plot_weights.csv')
 # USER: write you output location here
-output_location <- 'C:/Users/js81535/Desktop/lidar_scan_calibration/weighted_mean_and_std_biomasses_by_macroplot_and_type.csv'
+output_location <- 'C:/Users/js81535/Desktop/lidar_scan_calibration/csv_data/weighted_mean_and_std_biomasses_by_macroplot_and_type.csv'
 
-# combine the strata into one biomass per unique combination of clip plot and biomass type
+# combine the strata into one biomass value per unique combination of clip plot and biomass type
 low_df <- subset(input_data, Stratum == '0-30', select = c("Macroplot", "Clip.Plot", "X1000hr", "X100hr", "X10hr", "X1hr", "CL", "ETE", "FL", "PC", "PN", "Wlit.BL", "Wlive.BL"))
 high_df <- subset(input_data, Stratum == '30-100', select = c("Macroplot", "Clip.Plot", "X1000hr", "X100hr", "X10hr", "X1hr", "CL", "ETE", "FL", "PC", "PN", "Wlit.BL", "Wlive.BL"))
 by_clip_plot_df <- inner_join(low_df, high_df, by = c("Macroplot", "Clip.Plot"))
@@ -60,30 +60,36 @@ macroplot <- c(rep(1, 11), rep(2, 11), rep(3, 11))
 biomass_type <- c(rep(c("X1000hr", "X100hr", "X10hr", "X1hr", "CL", "ETE", "FL", "PC", "PN", "Wlit.BL", "Wlive.BL"), 3))
 weighted_mean_biomass <- c()
 weighted_standard_deviation <- c()
-# autocorrelation ranges are hard coded.
-autocorrelation_range <- c(rep(c(100, 2.1, 2.8, 5.1, 0.92, 3.2, 2.2, 7.1, 4.8, 0.64, 68), 3))
 
 # populate output columns
 for (i in 1:33) {              # there are 33 unique combinations of macroplot and biomass type
   
   weighted_biomass_values <- subset(weighted_masses_df, Macroplot == macroplot[[i]], select = biomass_type[[i]])
   weights <- subset(input_weights, Macroplot == macroplot[[i]], select = biomass_type[[i]])
-  this_mean <- sum(weighted_biomass_values) / sum(weights)         
+  if (sum(weights) == 0) {
+    this_mean <- 0          # prevent divide by zero
+  } else {
+    this_mean <- sum(weighted_biomass_values) / sum(weights)
+  }
   weighted_mean_biomass <- c(weighted_mean_biomass, this_mean)
   
   biomass_values <- subset(by_clip_plot_df, Macroplot == macroplot[[i]], select = biomass_type[[i]])
   weighted_squared_differences_from_mean <- weights * (biomass_values - this_mean)^2
-  variance <- (sum(weighted_squared_differences_from_mean)) / sum(weights)    
+  if (sum(weights) == 0) {
+    variance <- 0
+  } else {
+    variance <- (sum(weighted_squared_differences_from_mean)) / sum(weights)    
+  }
   standard_dev <- sqrt(variance)
   weighted_standard_deviation <- c(weighted_standard_deviation, standard_dev)
   
 }
 
 # build output data frame
-output <- data.frame(macroplot, biomass_type, weighted_mean_biomass, weighted_standard_deviation, autocorrelation_range)
+output <- data.frame(macroplot, biomass_type, weighted_mean_biomass, weighted_standard_deviation)
 
 print(output)
 
 # save the output to csv
-write.csv(output, output_location, row.names = FALSE)
+#write.csv(output, output_location, row.names = FALSE)
 
