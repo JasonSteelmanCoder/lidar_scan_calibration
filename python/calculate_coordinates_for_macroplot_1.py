7eab97cdc2de3cd6d35feef9ee2aac7709aa7634
Jason Steelman <jasonsteelmancoder@gmail.com>
2024-12-05 16:33:31 -0500

    start finding the coordinates of bounding boxes for clip plots

--- python/calculate_coordinates_for_macroplot_1.py
+++ python/calculate_coordinates_for_macroplot_1.py
@@ -1,5 +1,7 @@
 import math
 import matplotlib.pyplot as plt
+import re
+import numpy as np
 
 degrees_rotation = 104.8
 rotation_angle = degrees_rotation * (math.pi / 180)      # in radians
@@ -38,20 +40,90 @@ coordinate_pairs = [
 (-5,0),
 ]
 
-new_coordinates = []
+point_names = [
+    "e2",
+    "e2.5",
+    "e5",
+    "n2",
+    "n2.5",
+    "n5",
+    "ne3",
+    "ne4",
+    "ne6",
+    "nw3",
+    "nw4",
+    "nw6",
+    "s2",
+    "s2.5",
+    "s5",
+    "se3",
+    "se4",
+    "se6",
+    "sw3",
+    "sw4",
+    "sw6",
+    "w2",
+    "w2.5",
+    "w5"
+]
+
+new_macroplot = {}
 new_xs = []
 new_ys = []
+inner_wall_xs = []
+inner_wall_ys = []
+outer_wall_xs = []
+outer_wall_ys = []
 
-for point in coordinate_pairs:
-    row1 = a * point[0] + b * point[1]
-    row2 = c * point[0] + d * point[1]
+for i in range(24):
+    point = coordinate_pairs[i]
+
+    old_x = point[0]
+    old_y = point[1]
+
+    distance_from_origin = np.sqrt(old_x**2 + old_y**2)
+    
+    inner_wall_distance = distance_from_origin - 0.25
+    inner_wall_scaling_factor = inner_wall_distance / distance_from_origin
+    old_inner_wall_x = old_x * inner_wall_scaling_factor
+    old_inner_wall_y = old_y * inner_wall_scaling_factor
+
+    outer_wall_distance = distance_from_origin + 0.25
+    outer_wall_scaling_factor = outer_wall_distance / distance_from_origin
+    old_outer_wall_x = old_x * outer_wall_scaling_factor
+    old_outer_wall_y = old_y * outer_wall_scaling_factor
+
+    row1 = a * old_x + b * old_y
+    row2 = c * old_x + d * old_y
     new_point = (row1, row2)
-    new_coordinates.append(new_point)
+
+    inner_wall_x = a * old_inner_wall_x + b * old_inner_wall_y
+    inner_wall_y = c * old_inner_wall_x + d * old_inner_wall_y
+
+    outer_wall_x = a * old_outer_wall_x + b * old_outer_wall_y
+    outer_wall_y = c * old_outer_wall_x + d * old_outer_wall_y
+
+    new_macroplot[point_names[i]] = {
+        "name": point_names[i],
+        "x": row1,
+        "y": row2,
+        "inner_wall_x": inner_wall_x,
+        "inner_wall_y": inner_wall_y,
+        "outer_wall_x": outer_wall_x,
+        "outer_wall_y": outer_wall_y
+    }
     new_xs.append(row1)
     new_ys.append(row2)
+    inner_wall_xs.append(inner_wall_x)
+    inner_wall_ys.append(inner_wall_y)
+    outer_wall_xs.append(outer_wall_x)
+    outer_wall_ys.append(outer_wall_y)
 
-print(new_coordinates)
+for value in new_macroplot.values():
+    print(value)
 
 plt.scatter(new_xs, new_ys)
+plt.scatter(inner_wall_xs, inner_wall_ys, color='orange', alpha=0.33)
+plt.scatter(outer_wall_xs, outer_wall_ys, color='orange', alpha=0.33)
 plt.axis('equal')
 plt.show()
