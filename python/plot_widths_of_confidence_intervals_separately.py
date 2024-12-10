8fae143a1e0faa6bacff8fe9f6aa937f80e0754c
Jason Steelman <jasonsteelmancoder@gmail.com>
2024-12-10 12:17:57 -0500

    add optimal points to elbow curves

--- python/plot_widths_of_confidence_intervals_separately.py
+++ python/plot_widths_of_confidence_intervals_separately.py
@@ -2,6 +2,7 @@
 # of clip plots, along with a histogram showing the sample mean and the distribution of probable actual population means. The purpose of the figures is to give 
 # an intuitive sense of how precise (or inprecise) our estimations of biomass are.
 import matplotlib.pyplot as plt
+import matplotlib.gridspec as gridspec
 import numpy as np
 import os
 from dotenv import load_dotenv
@@ -14,21 +15,23 @@ input_data_path = f"C:/Users/{os.getenv("MS_USER_NAME")}/Desktop/lidar_scan_cali
 # USER: enter the location of the input folder here
 output_folder = f"C:/Users/{os.getenv("MS_USER_NAME")}/Desktop/lidar_scan_calibration/estimations_by_biomass_type"
 # USER: put the number of samples you want to simulate here
-samples_simulated = 5 
+samples_simulated = 3 
 
 input_data = pd.read_csv(input_data_path)
 # print(input_data)
 
 for i in range(36):
-    plt.figure()
+    fig = plt.figure(figsize=[9, 11])
+    gs = gridspec.GridSpec(3, 1)
 
     Z = 1.96            # 95% confidence interval
     sigma = input_data.iloc[i, 3]          # the standard deviation of the biomass on observed clip plots
 
     n = np.linspace(1, 24, 24)
     W = np.sqrt((4 * (Z**2) * (sigma**2)) / (n))
+    sum_xy = n + W
 
-    plt.subplot(1, 2, 1)            # put two plots in one panel
+    ax1 = fig.add_subplot(gs[0:2, :])
     plt.suptitle(f"{input_data.iloc[i, 1]} Macroplot {input_data.iloc[i, 0]}")
     plt.title(f"CI Width For Est'd Mean Biomass\nPer 1/4 m^2")  
 
@@ -38,14 +41,27 @@ for i in range(36):
         plt.plot(n, W, label = f"Macroplot {input_data.iloc[i, 0]}: {input_data.iloc[i, 1]}")
         plt.legend()
         if W[0] > 200:
-            plt.annotate("Note the high\nscale of uncertainty", (10, 1400))
+            plt.annotate("Note the high\nscale of uncertainty", (-1200, 2500))
+            plt.xticks([1, 24], rotation=270, fontsize='x-small')
+            plt.annotate("min(x + y) is not\nreached in 24 samples", (200, 700))
         else:
             plt.ylim([0, 130])
+            if W[0] > 70:
+                plt.xticks(range(0, 25, 4))
+            else:
+                plt.xticks(range(1, 24, 2))
+            # plt.plot(n, sum_xy)       # plots the guide function
+            optimal_x = sum_xy.argmin() + 1
+            optimal_y = W[sum_xy.argmin()]
+            plt.plot(optimal_x, optimal_y, 'bo')     # plots the tip of the elbow
+            plt.text(optimal_x + 1, optimal_y + 1, f"clip plots for\nmin(x + y): {int(np.round(optimal_x, 0))}")
+            plt.legend()
+            print(optimal_x, optimal_y)
         plt.ylabel("Width of Confidence Interval")
         plt.xlabel("Number of Clip Plots")
-        plt.xticks(range(1, 24, 2))
+        plt.axis('equal')
 
-    plt.subplot(1, 2, 2)            # the histogram goes in the second section of the panel
+    ax2 = fig.add_subplot(gs[2, :])
     plt.title(f"Distribution of Possible\nTrue Means With {samples_simulated} Clip Plots")
     if W[0] == 0:
         plt.text(0.5, 0.5, "No biomass found for this category", ha='center', va='center')
