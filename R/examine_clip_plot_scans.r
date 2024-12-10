908cdf5c03c0042d8182e081cbafcb5dbfd055a3
Jason Steelman <jasonsteelmancoder@gmail.com>
2024-12-10 12:18:37 -0500

    start normalizing macroplot scans

--- R/examine_clip_plot_scans.r
+++ R/examine_clip_plot_scans.r
@@ -1,6 +1,6 @@
 require(lidR)
 
-input_location <- 'C:/Users/js81535/Desktop/lidar_scan_calibration/clip_plot_las1/e2 .las'
+input_location <- 'C:/Users/js81535/Desktop/lidar_scan_calibration/clip_plot_las1/e2.las'
 
 las <- readLAS(input_location)
 
