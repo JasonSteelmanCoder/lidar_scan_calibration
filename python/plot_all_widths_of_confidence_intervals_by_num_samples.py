b9baf1accecb2a281af9d38b64fb2c2b28f98298
Jason Steelman <jasonsteelmancoder@gmail.com>
2024-12-10 16:55:44 -0500

    split totals out into their own plots

--- python/plot_all_widths_of_confidence_intervals_by_num_samples.py
+++ python/plot_all_widths_of_confidence_intervals_by_num_samples.py
@@ -1,5 +1,5 @@
-# This script makes three plots, each one representing a macroplot. On each plot there are several curves. Each curve represents the width of the confidence
-# interval for one type of biomass on that macroplot for each number of clip plots from 1 to 24. 
+# This script makes three plots, each one representing a macroplot. On each plot there are several curves. Each curve represents the margin of error for one 
+# type of biomass on that macroplot for each number of clip plots from 1 to 24. 
 import matplotlib.pyplot as plt
 import numpy as np
 import os
@@ -24,25 +24,65 @@ plt.figure()
 for i in range(3):
     for j in range(types_per_plot):
 
-        Z = 1.96            # 95% confidence interval
-        sigma = input_data.iloc[j + i * types_per_plot, 3]          # the standard deviation of the biomass on observed clip plots
+        biomass_type = input_data.iloc[j + i * types_per_plot, 1]
+        if biomass_type != 'total_biomass' and biomass_type != 'fine_dead_fuels':
 
-        n = np.linspace(1, 24, 24)
-        W = np.sqrt((4 * (Z**2) * (sigma**2)) / (n))            # calculate the width of a 95% confidence interval (note that half of that CI will be on each side of the estimated mean)
-        sum_xy = n + W
+            Z = 1.96            # 95% confidence interval
+            sigma = input_data.iloc[j + i * types_per_plot, 3]          # the standard deviation of the biomass on observed clip plots
 
-        plt.subplot(1, 3, i + 1)                 # make three plots on one panel
-        plt.plot(n, W, label = input_data.iloc[j + i * types_per_plot, 1], color=colors[j])
+            n = np.linspace(1, 24, 24)
+            margin_of_error = (Z * sigma) / np.sqrt(n)            # calculate half the width of a 95% confidence interval
+            if margin_of_error[1] != 0:
+                sum_xy = n + margin_of_error
 
-        optimal_x = sum_xy.argmin() + 1
-        optimal_y = W[sum_xy.argmin()]
-        plt.plot(optimal_x, optimal_y, 'o', color=colors[j])     # plots the tip of the elbow
+                plt.subplot(1, 3, i + 1)                 # make three plots on one panel
+                plt.plot(n, margin_of_error, label = input_data.iloc[j + i * types_per_plot, 1], color=colors[j])
 
-    plt.suptitle("CI Width For The Est'd Mean Biomass Per 1/4 m^2")
+                optimal_x = sum_xy.argmin() + 1
+                optimal_y = margin_of_error[sum_xy.argmin()]
+                plt.plot(optimal_x, optimal_y, 'o', color=colors[j], alpha=0.5)     # plots the tip of the elbow
+
+
+    plt.suptitle("Margin of Error For The Est'd Mean Biomass Per 1/4 m^2")
     plt.title(f"Macroplot {input_data.iloc[j + i * types_per_plot, 0]}")
     plt.legend()
     plt.ylim([0, 130])
-    plt.ylabel("Width of Confidence Interval")
+    plt.axis('equal')
+    plt.ylabel("Margin of Error")
+    plt.xlabel("Number of Clip Plots")
+    plt.xticks(range(1, 24, 2))
+
+plt.tight_layout(w_pad=-2)
+plt.show()
+
+plt.figure()
+for i in range(3):
+    for j in range(types_per_plot):
+
+        biomass_type = input_data.iloc[j + i * types_per_plot, 1]
+        if biomass_type == 'total_biomass' or biomass_type == 'fine_dead_fuels':
+
+            Z = 1.96            # 95% confidence interval
+            sigma = input_data.iloc[j + i * types_per_plot, 3]          # the standard deviation of the biomass on observed clip plots
+
+            n = np.linspace(1, 24, 24)
+            margin_of_error = (Z * sigma) / np.sqrt(n)            # calculate half the width of a 95% confidence interval
+            sum_xy = n + margin_of_error
+
+            plt.subplot(1, 3, i + 1)                 # make three plots on one panel
+            plt.plot(n, margin_of_error, label = input_data.iloc[j + i * types_per_plot, 1], color=colors[j])
+
+            optimal_x = sum_xy.argmin() + 1
+            optimal_y = margin_of_error[sum_xy.argmin()]
+            plt.plot(optimal_x, optimal_y, 'o', color=colors[j], alpha=0.5)     # plots the tip of the elbow
+
+
+    plt.suptitle("Margin of Error For The Est'd Mean Biomass Per 1/4 m^2")
+    plt.title(f"Macroplot {input_data.iloc[j + i * types_per_plot, 0]}")
+    plt.legend()
+    plt.ylim([0, 130])
+    plt.axis('equal')
+    plt.ylabel("Margin of Error")
     plt.xlabel("Number of Clip Plots")
     plt.xticks(range(1, 24, 2))
 
