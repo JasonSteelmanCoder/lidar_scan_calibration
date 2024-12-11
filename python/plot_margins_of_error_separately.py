1ace11edc4f77bec7646bdc699e32b3a5aed22a9
Jason Steelman <jasonsteelmancoder@gmail.com>
2024-12-11 09:55:33 -0500

    update separate elbow plots to show margin of error instead of width of confidence interval

--- python/plot_margins_of_error_separately.py
+++ python/plot_margins_of_error_separately.py
@@ -28,45 +28,45 @@ for i in range(39):
     sigma = input_data.iloc[i, 3]          # the standard deviation of the biomass on observed clip plots
 
     n = np.linspace(1, 24, 24)
-    W = np.sqrt((4 * (Z**2) * (sigma**2)) / (n))
-    sum_xy = n + W
+    margin_of_error = (Z * sigma) / np.sqrt(n)
+    sum_xy = n + margin_of_error
 
     ax1 = fig.add_subplot(gs[0:2, :])
     plt.suptitle(f"{input_data.iloc[i, 1]} Macroplot {input_data.iloc[i, 0]}")
-    plt.title(f"CI Width For Est'd Mean Biomass\nPer 1/4 m^2")  
+    plt.title(f"Margin of Error For Est'd Mean Biomass\nPer 1/4 m^2")  
 
-    if W[0] == 0:
+    if margin_of_error[0] == 0:
         plt.text(0.5, 0.5, "No biomass found for this category", ha='center', va='center')
     else:
-        plt.plot(n, W, label = f"Macroplot {input_data.iloc[i, 0]}: {input_data.iloc[i, 1]}")
+        plt.plot(n, margin_of_error, label = f"Macroplot {input_data.iloc[i, 0]}: {input_data.iloc[i, 1]}")
         plt.legend()
-        if W[0] > 200:
+        if margin_of_error[0] > 200:
             plt.annotate("Note the high\nscale of uncertainty", (-1200, 2500))
             plt.xticks([1, 24], rotation=270, fontsize='x-small')
             plt.annotate("min(x + y) is not\nreached in 24 samples", (200, 700))
         else:
             plt.ylim([0, 130])
-            if W[0] > 70:
+            if margin_of_error[0] > 70:
                 plt.xticks(range(0, 25, 4))
             else:
                 plt.xticks(range(1, 24, 2))
             # plt.plot(n, sum_xy)       # plots the guide function
             optimal_x = sum_xy.argmin() + 1
-            optimal_y = W[sum_xy.argmin()]
+            optimal_y = margin_of_error[sum_xy.argmin()]
             plt.plot(optimal_x, optimal_y, 'bo')     # plots the tip of the elbow
             plt.text(optimal_x + 1, optimal_y + 1, f"clip plots for\nmin(x + y): {int(np.round(optimal_x, 0))}")
             plt.legend()
             print(optimal_x, optimal_y)
-        plt.ylabel("Width of Confidence Interval")
+        plt.ylabel("Margin of Error")
         plt.xlabel("Number of Clip Plots")
         plt.axis('equal')
 
     ax2 = fig.add_subplot(gs[2, :])
     plt.title(f"Distribution of Possible\nTrue Means With {samples_simulated} Clip Plots")
-    if W[0] == 0:
+    if margin_of_error[0] == 0:
         plt.text(0.5, 0.5, "No biomass found for this category", ha='center', va='center')
     else:
-        prediction_distribution = np.random.normal(input_data.iloc[i, 2], W[samples_simulated - 1] / 4, 1000)
+        prediction_distribution = np.random.normal(input_data.iloc[i, 2], margin_of_error[samples_simulated - 1] / 2, 1000)
         plt.hist(prediction_distribution, bins=50)
         plt.xlabel("Mean Biomass")
         plt.ylabel("Count")
@@ -77,7 +77,7 @@ for i in range(39):
     plt.tight_layout()
 
     # save each figure to its own png image file
-    file_name = f"confidence_interval_macroplot{input_data.iloc[i, 0]}_{input_data.iloc[i, 1].replace(".", "")}"
+    file_name = f"margin_of_error_macroplot{input_data.iloc[i, 0]}_{input_data.iloc[i, 1].replace(".", "")}"
     plt.savefig(os.path.join(output_folder, file_name))
     plt.close()
 
