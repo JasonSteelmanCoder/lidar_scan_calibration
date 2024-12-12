0d3189dde178531bc5a4698760eab745458e1377
Jason Steelman <jasonsteelmancoder@gmail.com>
2024-12-12 10:32:39 -0500

    adjust variogram panels to centers of clip plots

--- R/semivariogram_automap_2.r
+++ R/semivariogram_automap_2.r
@@ -6,11 +6,21 @@ library(png)
 library(magick)
 library(glue)
 
-input.csv <- 'C:/Users/js81535/Desktop/lidar_scan_calibration/HEF Biomass 2024.csv'
+input.csv <- 'C:/Users/js81535/Desktop/lidar_scan_calibration/csv_data/HEF Biomass 2024.csv'
 file_data <- read.csv(input.csv)
 
-file_data$X <- as.numeric(str_extract(file_data$Coordinates, "(-)?\\d+(\\.\\d+)?"))
-file_data$Y <- as.numeric(str_extract(file_data$Coordinates, "((-)?\\d+(\\.\\d+)?)\\]", group=1))
+# extract the edge coordinates from the names of the clip plots
+file_data$nominal_X <- as.numeric(str_extract(file_data$Coordinates, "(-)?\\d+(\\.\\d+)?"))
+file_data$nominal_Y <- as.numeric(str_extract(file_data$Coordinates, "((-)?\\d+(\\.\\d+)?)\\]", group=1))
+
+# make the edge coordinates into the center coordinates for each clip plot
+file_data$magnitude <- sqrt(file_data$nominal_X^2 + file_data$nominal_Y^2)
+file_data$multiples_of_quarter <- file_data$magnitude / 0.25
+file_data$quarter_X <- file_data$nominal_X / file_data$multiples_of_quarter
+file_data$quarter_Y <- file_data$nominal_Y / file_data$multiples_of_quarter
+file_data$X <- file_data$nominal_X + file_data$quarter_X
+file_data$Y <- file_data$nominal_Y + file_data$quarter_Y
+
 
 macroplot1_low <- subset(file_data, Macroplot == 1 & Stratum == '0-30')
 macroplot2_low <- subset(file_data, Macroplot == 2 & Stratum == '0-30')
@@ -114,4 +124,5 @@ for (type in biomass_types) {
 #dev.new()
 #dev.set(2)
 #dev.off()
+
 
