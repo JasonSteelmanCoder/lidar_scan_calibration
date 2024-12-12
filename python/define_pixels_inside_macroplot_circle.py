f4e0325feffd6a6e6f57319a007b6ef4cb51f640
Jason Steelman <jasonsteelmancoder@gmail.com>
2024-12-12 15:49:20 -0500

    find the corners of all squares that are completely inside the radius

--- python/define_pixels_inside_macroplot_circle.py
+++ python/define_pixels_inside_macroplot_circle.py
@@ -6,11 +6,22 @@ radius = 10
 x = np.linspace(0, radius)
 y = np.sqrt(radius**2 - x**2)
 plt.plot(x, y)
+quad1_corners = []
+quad2_corners = []
+quad3_corners = []
+quad4_corners = []
 for xi in range(1, 11):
     for yi in range(1, 11):
         if yi <= np.sqrt(radius**2 - xi**2):
-            print(xi, yi)
-            plt.plot(xi, yi, 'o')
+            quad1_corners.append((xi, yi))
+            quad2_corners.append((-xi, yi))
+            quad3_corners.append((-xi, -yi))
+            quad4_corners.append((xi, -yi))
+            plt.plot(xi, yi, 'bo')
 plt.xlim(left=0)
 plt.axis('equal')
-plt.show()
+# plt.show()
+print(quad1_corners[-1])
+print(quad2_corners[-1])
+print(quad3_corners[-1])
+print(quad4_corners[-1])
