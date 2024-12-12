7a9f231104a8dc0ebed76c1f6acd09fd7a661267
Jason Steelman <jasonsteelmancoder@gmail.com>
2024-12-12 16:49:20 -0500

    find pixels for the whole circle

--- python/define_pixels_inside_macroplot_circle.py
+++ python/define_pixels_inside_macroplot_circle.py
@@ -2,6 +2,7 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 radius = 10
+pixel_width = 0.5
 
 x = np.linspace(0, radius)
 y = np.sqrt(radius**2 - x**2)
@@ -10,18 +11,104 @@ quad1_corners = []
 quad2_corners = []
 quad3_corners = []
 quad4_corners = []
-for xi in range(1, 11):
-    for yi in range(1, 11):
+for xi in np.arange(pixel_width, 11, pixel_width):
+    for yi in np.arange(pixel_width, 11, pixel_width):
         if yi <= np.sqrt(radius**2 - xi**2):
             quad1_corners.append((xi, yi))
             quad2_corners.append((-xi, yi))
             quad3_corners.append((-xi, -yi))
             quad4_corners.append((xi, -yi))
             plt.plot(xi, yi, 'bo')
-plt.xlim(left=0)
-plt.axis('equal')
+# print(quad1_corners[0])
+# print(quad2_corners[0])
+# print(quad3_corners[0])
+# print(quad4_corners)
+# plt.xlim(left=0)
+# plt.axis('equal')
 # plt.show()
-print(quad1_corners[-1])
-print(quad2_corners[-1])
-print(quad3_corners[-1])
-print(quad4_corners[-1])
+
+pixels = []
+
+for corner in quad1_corners:
+    top_right_x = corner[0]
+    top_right_y = corner[1]
+    top_left_x = top_right_x - pixel_width
+    top_left_y = top_right_y
+    bottom_left_x = top_left_x
+    bottom_left_y = top_right_y - pixel_width
+    bottom_right_x = top_right_x
+    bottom_right_y = bottom_left_y
+
+    pixel = (
+        (top_right_x, top_right_y), 
+        (top_left_x, top_left_y),
+        (bottom_left_x, bottom_left_y),
+        (bottom_right_x, bottom_right_y)
+    )
+    pixels.append(pixel)
+
+
+for corner in quad2_corners:
+    top_left_x = corner[0]
+    top_left_y = corner[1]
+    bottom_left_x = top_left_x
+    bottom_left_y = top_left_y - pixel_width
+    bottom_right_x = bottom_left_x + pixel_width
+    bottom_right_y = bottom_left_y
+    top_right_x = top_left_x + pixel_width
+    top_right_y = top_left_y
+
+    pixel = (
+        (top_right_x, top_right_y), 
+        (top_left_x, top_left_y),
+        (bottom_left_x, bottom_left_y),
+        (bottom_right_x, bottom_right_y)
+    )
+    pixels.append(pixel)
+
+
+for corner in quad3_corners:
+    bottom_left_x = corner[0]
+    bottom_left_y = corner[1]
+    bottom_right_x = bottom_left_x + pixel_width
+    bottom_right_y = bottom_left_y
+    top_right_x = bottom_right_x
+    top_right_y = bottom_left_y + pixel_width
+    top_left_x = bottom_left_x
+    top_left_y = top_right_y
+
+    pixel = (
+        (top_right_x, top_right_y), 
+        (top_left_x, top_left_y),
+        (bottom_left_x, bottom_left_y),
+        (bottom_right_x, bottom_right_y)
+    )
+    pixels.append(pixel)
+
+
+for corner in quad4_corners:
+    bottom_right_x = corner[0]
+    bottom_right_y = corner[1]
+    top_right_x = bottom_right_x
+    top_right_y = bottom_right_y + pixel_width
+    top_left_x = bottom_right_x - pixel_width
+    top_left_y = bottom_right_y + pixel_width
+    bottom_left_x = top_left_x
+    bottom_left_y = bottom_right_y
+
+    pixel = (
+        (top_right_x, top_right_y), 
+        (top_left_x, top_left_y),
+        (bottom_left_x, bottom_left_y),
+        (bottom_right_x, bottom_right_y)
+    )
+    pixels.append(pixel)
+
+
+for pixel in pixels:
+    xs = [pixel[0][0], pixel[1][0], pixel[2][0], pixel[3][0], pixel[0][0]]
+    ys = [pixel[0][1], pixel[1][1], pixel[2][1], pixel[3][1], pixel[0][1]]
+    plt.plot(xs, ys)
+plt.axis('equal')
+plt.show()
+
