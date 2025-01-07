21c69cd18b54529e70189db4e4e82ce13ae07f3a
Jason Steelman <jasonsteelmancoder@gmail.com>
2025-01-07 10:31:42 -0500

    add doc strings

--- R/multiplot_all_strata_variograms.r
+++ R/multiplot_all_strata_variograms.r
@@ -1,4 +1,4 @@
-# This program makes single variograms that combine all strata and all macroplots.
+# This program makes single variograms for each biomass type that combine all strata and all macroplots.
 # It has been adjusted for clip plot centers
 
 library(automap)
@@ -9,9 +9,11 @@ library(png)
 library(magick)
 library(glue)
 
+## USER: put the path to your input file here
 input.csv <- 'C:/Users/js81535/Desktop/lidar_scan_calibration/csv_data/HEF Biomass 2024 multiplot.csv'
 file_data <- read.csv(input.csv)
 
+## combine high and low strata into single values for each biomass type
 all_strata_data <- file_data %>% 
   group_by(Macroplot, Clip.Plot, Coordinates, multiplot_x, multiplot_y) %>%
   summarize(
@@ -30,39 +32,62 @@ all_strata_data <- file_data %>%
   )
 print(all_strata_data)
 
-
+## grab the names of all the biomass types
 biomass_types <- names(file_data)[5:15]
-# print(biomass_types)
 
+## loop through all the boimass types
 for (type in biomass_types) {
   
   biomass_type = type
   
+  ## make a spatial data frame for the current biomass type
+  ## the data frame will have columns for x, y, and biomass
   spatial <- SpatialPointsDataFrame(
     coords = all_strata_data[, c("multiplot_x", "multiplot_y")],
     data = subset(all_strata_data, select = biomass_type)
   )
   
-  
+  ## there is only one spatial data frame per iteration
+  ## but we put them in a list because this program is adapted from 
+  ## a program where there were several
   plot_layers = c(spatial)
   plot_layer_names = c("all_strata_")
   
+  ## this loop is really just running the code once for the spatial data frame on this iteration
   for (i in 1:1) {
+    
+    ## make a formula like "ete ~ 1" for the variogram model to use
     data_name <- names(plot_layers[[i]])[1]
     formula <- as.formula(paste(data_name, '~ 1'))
     
+    ## make a name for the png file
     clean_data_name <- glue(plot_layer_names[[i]], gsub("\\.", "", data_name))
+    
+    ## start a device that can write a png file
     png(filename = paste(clean_data_name, ".png", sep = ''))
+    
+    ## calculate a variogram
     variogram = autofitVariogram(formula = formula, input_data = plot_layers[[i]], verbose = FALSE, miscFitOptions = list(merge.small.bins = TRUE))
+    
+    ## tryCatch is used here in case there is no data for a certain biomass type
+    ## the "error" function will run if there is no data
     tryCatch({
+      
       the_plot <- plot(variogram, multipanel = TRUE)
+      ## customize the name of the variogram plot
       the_plot$main = clean_data_name
+      ## save the plot to png
       plot(the_plot)
+      
     }, error = function(e) {
+      
+      ## make a "plot" that says that there is no data for this biomass type
       plot(1, 1, main = clean_data_name, type = 'n', ylab = "", xlab = "")
       text(x = 1, y = 1, labels = "biomass not found for this category", col = 'darkgrey')
-      #print(e)
+
     })
+
+    ## exit from the device that writes png files    
     dev.off()
     
   }
